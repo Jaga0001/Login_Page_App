2caa366998bce3a2a1d96b1dfd387adde1cd534e
Jaga0001 <144882407+Jaga0001@users.noreply.github.com>
2024-06-26 23:17:22 +0530

    June 26 Work

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,22 +15,46 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
 
   final _emailController = TextEditingController();
-
   final _passwordController =  TextEditingController();
-
   final _confirmPasswordController = TextEditingController();
 
+  //* Creating Addtional Fields For Database
+
+  final _firstNameController = TextEditingController();
+  final _lastNameController  = TextEditingController();
+  final _ageController = TextEditingController();
   final snackBar = SnackBar(content: Text('Password are not Same'));
 
   Future signup() async {
     if (paswwordConfirmed()) {
+
+      //* Create User
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: _emailController.text.trim(), 
         password: _passwordController.text.trim(),
         );
+
+      //* Add User Details
+      addUserDetails(
+        _firstNameController.text.trim(), 
+        _lastNameController.text.trim(), 
+        _emailController.text.trim(), 
+        int.parse(_ageController.text.trim())
+      );
+
+
     } else {
       return ScaffoldMessenger.of(context).showSnackBar(snackBar);
     }
+  }
+
+  Future addUserDetails(String firstName, lastName, email, int age) async{
+    await FirebaseFirestore.instance.collection('users').add({
+      'First Name': firstName,
+      'Last Name':  lastName,
+      'Age':  age,
+      'Email': email, 
+    });
   }
 
   bool paswwordConfirmed() {
@@ -45,6 +70,9 @@ class _RegisterPageState extends State<RegisterPage> {
     // TODO: implement dispose
     _emailController.dispose();
     _passwordController.dispose();
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _ageController.dispose();
     super.dispose();
   }
 
@@ -59,8 +87,7 @@ class _RegisterPageState extends State<RegisterPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.android, size: 100,),
-                SizedBox(height: 25),
+                
                 //Hello Again
                 // Text('Hello Again!',
                 // style: GoogleFonts.bebasNeue(
@@ -85,7 +112,78 @@ class _RegisterPageState extends State<RegisterPage> {
                   ),
                 ),
                 SizedBox(height: 50),
-            
+
+
+
+                // *First Name TextField
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        controller: _firstNameController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'First Name'
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                // *Last Name TextField
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        controller: _lastNameController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Last Name'
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                // *Age TextField
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        controller: _ageController,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Enter Age'
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+                
                 // *Email TextField
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25.0),
